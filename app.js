5931de80a0b2729fe71005fd65662ceecc20abfd
十七号 <1065890063@qq.com>
2015-08-06 18:13:23 +0800

    cors

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var musics = require('./routes/musics');
 
 var app = express();
-
+app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
